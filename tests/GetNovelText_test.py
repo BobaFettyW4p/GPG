9885439a4defebbdc67b47605802af6a8a0c09cc
BobaFettyW4p <matthew.ivancic91@gmail.com>
2023-01-03 11:12:27 -0600

    updated tests

--- tests/GetNovelText_test.py
+++ tests/GetNovelText_test.py
@@ -1,13 +1,19 @@
-import sys, string
+import sys
 sys.path.insert(0,'./app/')
 
-import pytest, gutenbergpy.textget, GetNovelText
+import string, pytest, gutenbergpy.textget, GetNovelText
 
 
 @pytest.fixture
 def sample_text():
     book = gutenbergpy.textget.get_text_by_id(890)
     return book
+
+@pytest.fixture
+def sample_dictionary():
+    book = GetNovelText.get_book(890)
+    final_book = GetNovelText.clean_book(book)
+    return final_book
 
 def test_get_book(sample_text):
     assert type(GetNovelText.get_book(890)) is bytes
@@ -28,8 +34,30 @@ def test_capitalizaton_clean_book(sample_text):
     book = GetNovelText.clean_book(sample_text)
     valid = True
     for page in book:
-        if page[0] not in string.ascii_uppercase:
+        if page[0] in string.ascii_lowercase:
             valid = False
             break
-    assert valid == True
-    
+    assert valid
+
+def test_clean_book_word_length(sample_text):
+    book = GetNovelText.clean_book(sample_text)
+    valid = True
+    for word in book:
+        if len(word) <= 3:
+            valid = False
+            break
+    assert valid
+
+def test_clean_book_uniqueness(sample_text):
+    book = GetNovelText.clean_book(sample_text)
+    valid = True
+    for word in book:
+        if book.count(word) != 1:
+            valid = False
+            break
+    assert valid
+
+def test_generate_password_candidates(sample_dictionary):
+    PASSWORD_LENGTH = 20
+    candidates = GetNovelText.generate_password(sample_dictionary,PASSWORD_LENGTH)
+    assert len(candidates) == 5
