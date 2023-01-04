46a7e4eaf2c0f3e3e390d4b7730fada6cda183db
BobaFettyW4p <matthew.ivancic91@gmail.com>
2023-01-04 13:53:54 -0600

    adding setup.py

---
+++ app/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = 'PecanTiger',
+    version = '1.0.0',
+    author = 'Matthew Ivancic',
+    author_email = 'matthew.ivancic91@gmail.com',
+    url = 'https://github.com/BobaFettyW4p/PecanTiger',
+    description = 'password generation program utilizing GutenbergPY and Textual',
+)
